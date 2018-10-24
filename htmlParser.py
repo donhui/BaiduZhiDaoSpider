f48becef6738ef5fc03f8087b8158806a4cbb1d7
donhui <977675308@qq.com>
2018-10-24 12:49:03 +0800

    增加 回答时间

--- htmlParser.py
+++ htmlParser.py
@@ -71,12 +71,16 @@ class HtmlParser(object):
                 tmp_evaluate_terrible_num = tmp_etree.xpath(bad_rules)[0]
                 tmp_answer_content = all_content.split('</div>')[-2]
 
+                reply_date_rules='//*[@id="wgt-replyer-all-'+tmp_id+'"]/span/span[last()]'
+                reply_date = tmp_etree.xpath(reply_date_rules)[0].text.replace("推荐于","")
+
                 is_best_answer = "最佳答案"
 
-                # ['类别', '问题', '问题详情', '回答总数', '回答', '点赞数', '踩数'，'最佳答案']
+                # ['类别', '问题', '问题详情', '回答总数', '回答', '回答时间','点赞数', '踩数'，'最佳答案']
 
-                tmp_information = [tmp_category, tmp_question, tmp_question_detail, answers_count, tmp_answer_content, tmp_evaluate_good_num,
-                                   tmp_evaluate_terrible_num, is_best_answer]
+                tmp_information = [tmp_category, tmp_question, tmp_question_detail, answers_count,
+                                   tmp_answer_content, reply_date,
+                                   tmp_evaluate_good_num,tmp_evaluate_terrible_num, is_best_answer]
                 self.datas.append(tmp_information)
 
             # 普通答案
@@ -93,6 +97,9 @@ class HtmlParser(object):
                         tmp_evaluate_terrible_num = tmp_etree.xpath(bad_rules)[0]
                         tmp_answer_content = all_content.split('</div>')[-2]
 
+                        reply_date_rules = '//*[@id="wgt-replyer-all-' + tmp_id + '"]/span/span[last()]'
+                        reply_date = tmp_etree.xpath(reply_date_rules)[0].text.replace("推荐于","")
+
                         if (len(best_answer_id) != 0):
                             tmp_question = ""
                             tmp_question_detail = ""
@@ -106,7 +113,8 @@ class HtmlParser(object):
 
                         # ['类别','问题', '问题详情', '回答','点赞数','踩数', '最佳答案']
 
-                        tmp_information=[tmp_category, tmp_question, tmp_question_detail, answers_count, tmp_answer_content,
-                                         tmp_evaluate_good_num,tmp_evaluate_terrible_num, is_best_answer]
+                        tmp_information = [tmp_category, tmp_question, tmp_question_detail, answers_count,
+                                           tmp_answer_content, reply_date,
+                                           tmp_evaluate_good_num, tmp_evaluate_terrible_num, is_best_answer]
                         self.datas.append(tmp_information)
         return self.datas
